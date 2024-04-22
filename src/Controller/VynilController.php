114fbd4f9f2996ade2a7dc3f54bd76e0e784266b
Ayder Umerov <ayderumerov.com@gmail.com>
2024-04-22 13:43:35 +0200

    Added some Twiggy goodness

--- src/Controller/VynilController.php
+++ src/Controller/VynilController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use function Symfony\Component\String\u;
 
-class VynilController
+class VynilController extends AbstractController
 {
     #[Route('/')]
     public function homepage()
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Test', 'artist' => 'Test'],
+            ['song' => 'Test', 'artist' => 'Test'],
+            ['song' => 'Test', 'artist' => 'Test'],
+            ['song' => 'Test', 'artist' => 'Test'],
+            ['song' => 'Test', 'artist' => 'Test'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB and Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
